64a110d32238c359aed34324ecea1866edc12a3e
Ivan <ivan.bondarenko.it@gmail.com>
2023-04-20 14:24:01 +0400

    first api request

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ def get_api(city_name, country_code):
         "appid": api_key,
     }
 
-    return api_manage.ApiManage("https://api.openweathermap.org/data/2.5/weather", params=params).get_json()
+    params_one_call = {
+        "lat": 41.6416,
+        "lon": 41.6359,
+        # "exclude": "",
+        "appid": api_key,
+    }
+
+    # api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+    # ?lat = {lat} & lon = {lon} & exclude = {part} & appid = {API key}
+
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    url_one_call = "https://api.openweathermap.org/data/2.5/forecast"
+
+    return api_manage.ApiManage(url_one_call, params=params_one_call).get_json()
 
 
 if __name__ == '__main__':
