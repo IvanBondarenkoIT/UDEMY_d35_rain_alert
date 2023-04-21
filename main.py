5613cd5ecb3668c5b8b1118b03fa66ea26c66098
Ivan <ivan.bondarenko.it@gmail.com>
2023-04-21 22:02:51 +0400

    print("Bring an umbrella.")

--- main.py
+++ main.py
@@ -3,30 +3,29 @@ import api_manage
 
 def get_api(city_name, country_code):
     api_key = "976e8d42a3ba638fb1d73cea82cdd613"
+    # "{'lon': 41.6359, 'lat': 41.6416}" "Batumi"
     params = {
-        "q": f"{city_name},{country_code}",
-        "appid": api_key,
-    }
-
-    params_one_call = {
         "lat": 41.6416,
         "lon": 41.6359,
         # "exclude": "",
         "appid": api_key,
     }
 
-    # api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    url = "https://api.openweathermap.org/data/2.5/forecast"
 
-    # ?lat = {lat} & lon = {lon} & exclude = {part} & appid = {API key}
-
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    url_one_call = "https://api.openweathermap.org/data/2.5/forecast"
-
-    return api_manage.ApiManage(url_one_call, params=params_one_call).get_json()
+    return api_manage.ApiManage(url, params=params).get_json()
 
 
 if __name__ == '__main__':
-    print(get_api("Batumi", "GE"))
+    json_data = get_api('Batumi', 'GE')
+    will_rain = False
+    for hour_data in json_data["list"][:9]:
+        condition_code = hour_data['weather'][0]['id']
+        if condition_code < 700:
+            will_rain = True
+            # print(hour_data['dt_txt'], hour_data['weather'][0]['main'])
 
-    # "{'lon': 41.6359, 'lat': 41.6416}"
+    if will_rain:
+        print("Bring an umbrella.")
+
 
